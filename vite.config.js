1b3698b025728be834d9e1d671334c3bef874722
Avinashpasham07 <avinashpasham2004@gmail.com>
2025-11-14 23:02:23 +0530

    fix: tailwind build error

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss({
-      config: {
-        darkMode: "class", // ✅ Enables Dark Mode toggle
-        theme: {
-          extend: {},
-        },
-      },
-    }),
+    tailwindcss(),
   ],
-});
+})
